06c54c52a98b3121962dbf5a24c2445cf2b3d9c0
vikonik <vikonik@yandex.ru>
2021-09-05 21:58:34 +0300

    Leson_2

--- app/src/main/java/com/example/leson_1/MainActivity.java
+++ app/src/main/java/com/example/leson_1/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.leson_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+   private ColorSpec spec = new ColorSpec();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
+
     }
+
+    public void onClickColorEffect(View view) {
+        Log.d("MainActivity", "onClickColorEffect");
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        TextView text2 = (TextView) findViewById(R.id.textView2);//нейминг!!!
+        String color = String.valueOf(spinner.getSelectedItem());
+        String result = spec.getEffect(color);//getEffect это функция класса  ColorSpec
+        text2.setText(result);
+    }
+
 }
